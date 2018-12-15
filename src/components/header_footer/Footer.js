a39955e5a9cfc8fb2c78be38764f29be56ce45d0
user <shaktibhujel06@gmail.com>
2018-12-15 12:12:18 +0545

    footer section added

---
+++ src/components/header_footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Fade from "react-reveal/Fade";
+
+const footer = () => {
+  return (
+    <footer className="bck_black">
+      <Fade delay={500}>
+        <div className="footer_copyright">Venue@2018.All Rights Reserved</div>
+      </Fade>
+    </footer>
+  );
+};
+
+export default footer;
