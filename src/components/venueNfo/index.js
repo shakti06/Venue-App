7bf473751b88ea248a303d02a3213d3e816393a6
user <shaktibhujel06@gmail.com>
2018-12-15 10:52:08 +0545

    place details and location added

---
+++ src/components/venueNfo/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Zoom from "react-reveal/Zoom";
+import calendar from "../../resources/images/icons/calendar.png";
+import location from "../../resources/images/icons/location.png";
+
+const VenueNfo = () => {
+  return (
+    <div className="bck_black">
+      <div className="center_wrapper">
+        <div className="vn_wrapper">
+          <Zoom duration={500}>
+            <div className="vn_item">
+              <div className="vn_outer">
+                <div className="vn_inner">
+                  <div className="vn_icon_square bck_red" />
+                  <div
+                    className="vn_icon"
+                    style={{ background: `url(${calendar})` }}
+                  />
+                  <div className="vn_title">Event Date & Time</div>
+                  <div className="vn_desc">30 Dec, 2018</div>
+                </div>
+              </div>
+            </div>
+          </Zoom>
+
+          <Zoom duration={500} delay={500}>
+            <div className="vn_item">
+              <div className="vn_outer">
+                <div className="vn_inner">
+                  <div className="vn_icon_square bck_yellow" />
+                  <div
+                    className="vn_icon"
+                    style={{ background: `url(${location})` }}
+                  />
+                  <div className="vn_title">Event Date & Time</div>
+                  <div className="vn_desc">30 Dec, 2018</div>
+                </div>
+              </div>
+            </div>
+          </Zoom>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VenueNfo;
