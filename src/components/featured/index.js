a94c96774178bfa8ab25ed73271dcd05fc5f37b3
user <shaktibhujel06@gmail.com>
2018-12-15 10:50:57 +0545

    featured section added

---
+++ src/components/featured/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Carousel from "./Carousel";
+import TimeUntil from "./TimeUntil";
+
+const Featured = () => {
+  return (
+    <div style={{ position: "relative" }}>
+      <Carousel />
+      <div className="artist_name">
+        <div className="wrapper">ADELE</div>
+      </div>
+      <TimeUntil />
+    </div>
+  );
+};
+
+export default Featured;
